b44e52042db544ed37d9c0eb0d7ba90a535d86c9
Mouradez <mourad.EZZEBDI@um6p.ma>
2021-01-20 15:41:02 +0100

    Change

---
+++ exercices_c/func.c
@@ -0,0 +1,9 @@
+#include<stdlib.h>
+#include<stdio.h>
+int somme(int a, int b)
+{
+	return (a+b);
+}
+
+
+
---
+++ exercices_c/main.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include "fichier.h"
+int main()
+{
+	int a=2;
+	int b=2;
+	int z=somme(a,b);
+	printf("%d\n",z);
+	return 0;
+
+}
+
